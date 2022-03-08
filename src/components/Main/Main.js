b22a20617883fccef21f6246c8e068fded616be8
BrendenSeifried <brendenseifried@gmail.com>
2022-03-07 17:23:00 -0800

    got a background working! had to change apps js not sure if i was supposed to

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Main.css';
-import '../ZodiacCard/ZodiacCard.css';
+// import '../ZodiacCard/ZodiacCard.css';
 
 import background from './background.png';
-import Zodiac from '../ZodiacCard/ZodiacCard.js';
-import { zodiacs } from '../../data.js';
+// import Zodiac from '../ZodiacCard/ZodiacCard.js';
+// import { zodiacs } from '../../data.js';
 
 export default function Main() {
   return (
-    
+    <div className="main" style={{ backgroundImage: `url(${ background })` }}>
+      <main />
+    </div>
   );
 }
